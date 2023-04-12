d45bdafb7a18431a359ea982b218002803c08d7a
eborgesAO <eloim.borges.ao@gmail.com>
2023-04-12 15:16:44 -0400

    Finished organization of the code by functions

--- verification.py
+++ verification.py
@@ -14,10 +14,6 @@ def main():
             #it takes the variable, lo divide en tab y lo guarda en elments en una lista
             elements = line.split("\t")
             
-            # Program Variables
-            # results_output = "/share/s3453g1/keysha/Development/ISR/ISR_Program/output/prog_output_dir.txt"
-            # results_input = "/share/s3453g1/keysha/Development/ISR/ISR_Program/output/prog_input_dir.txt"
-            
             results_output = "outputs/output_dir_verification.txt"
             results_input = "outputs/input_dir_verification.txt"
         
@@ -61,6 +57,7 @@ def find_missing_numbers(arr):
     # Return the missing numbers
     return summary
 
+#Run the verification on the inputs directories and write the results summary
 def verify_input(input_files, results_input):
     #start of the input code
     input_identification = []
@@ -89,22 +86,7 @@ def verify_input(input_files, results_input):
         file.write("Maximum number: " + str(extra_detail[1]) + "\n")
         file.write("Missing numbers: " + str(extra_detail[2]) + "\n")
 
-def verify_output(results_output, output_files):
-    output_extensions = {}
-    for file in output_files:
-        output_files = file.split(".")
-        out_type = output_files[1] #file type
-        time_ext = output_files[2] #time_ext
-        profile_ext = output_files[3] #Ext 2
-        # add profile extension to output_extensions[out_type][time_ext] as an append to list
-        if out_type not in output_extensions:
-            output_extensions[out_type] = {}
-        if time_ext not in output_extensions[out_type]:
-            output_extensions[out_type][time_ext] = []
-        output_extensions[out_type][time_ext].append(profile_ext)
-    
-    out_type_list = list(output_extensions.keys())
-    
+def criteria_1(out_type_list,results_output):
     passed =True
     # has clppi
     # has toppi
@@ -127,31 +109,32 @@ def verify_output(results_output, output_files):
     #update the variable passed
     if not one_instance:
         passed = False
-#  if condition is met write to file
-    breakpoint()
+    #if condition is met write to file
     if passed == False:
         with open(results_output, "a") as file:
-            file.write("\n Extra Details: \n\n")
+            file.write("Extra Details: \n\n")
             file.write("File doesn't only have clppi and toppi. It contains: " + str(out_type_list))
-        return
+    return passed
 
-#----------------------------------------------------------------------------------------
+def criteria_2(output_extensions,results_output):
+
     time_ext_list = list(output_extensions["toppi_1"].keys())
     time_ext_list = [int(x) for x in time_ext_list]
     results=find_missing_numbers(time_ext_list)
-#  find missing numbers
     if len(results[2]) != 0:
         with open(results_output, "a") as file:
             file.write("\nTheres a missing numbers in Toppi: " + str(results))
-    
+
+    #toppi/\    &   clppi \/
+
     time_ext_list = list(output_extensions["clppi_1"].keys())
     time_ext_list = [int(x) for x in time_ext_list]
     results=find_missing_numbers(time_ext_list)
-# if theres a missing number
     if len(results[2]) != 0:
         with open(results_output, "a") as file:
             file.write("\nTheres a missing numbers in Clppi: " + str(results))
 
+def criteria_3_4(output_extensions, results_output):
 # for each category in output_extensions
     for category in output_extensions.keys():
         ext_1_len = len(output_extensions[category].keys())
@@ -161,23 +144,51 @@ def verify_output(results_output, output_files):
             ext2_list = output_extensions[category][ext1]
             ext2_list = [int(x) for x in ext2_list]
             results=find_missing_numbers(ext2_list)
+            #Here we check for criteria #3
             if i == 0:
                 min_num = results[0]
                 max_num = results[1]
             
-            
+            #If anomaly is found in criteria #3 write it to file
             if len(results[2]) != 0:
                 with open(results_output, "a") as file:
                     file.write("\n\nTheres a missing numbers \n")
                     failed = True
+            #Here we verify criteria #4
             elif (results[0] != min_num or results[1] != max_num) and i != last_i:
                 with open(results_output, "a") as file:
                     file.write("\n\nTheres a different mins and max than the rest \n")
                     file.write(f"first min and max: {min_num} {max_num} \n")
                     file.write(f"min and max found: {results[0]} {results[1]} \n")
                     failed = True
-            
+            #If found a different mins and max ext write to file
             if failed:
                 with open(results_output, "a") as file:
                     file.write(str(category)+ " " +str(ext1)+ str(results))
+
+def verify_output(results_output, output_files):
+    output_extensions = {}
+    for file in output_files:
+        output_files = file.split(".")
+        out_type = output_files[1] #file type
+        time_ext = output_files[2] #time_ext
+        profile_ext = output_files[3] #Ext 2
+        # add profile extension to output_extensions[out_type][time_ext] as an append to list
+        if out_type not in output_extensions:
+            output_extensions[out_type] = {}
+        if time_ext not in output_extensions[out_type]:
+            output_extensions[out_type][time_ext] = []
+        output_extensions[out_type][time_ext].append(profile_ext)
+    
+    out_type_list = list(output_extensions.keys())
+    # Call the function and store the result in a variable
+    passed = criteria_1(out_type_list, results_output)
+    if passed == False:
+        return
+    criteria_2(output_extensions,results_output)
+
+    criteria_3_4(output_extensions, results_output)
+
+
+
 main()
